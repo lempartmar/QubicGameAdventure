5b07fe97c7421640f2f3fa8f98b0d282ff673773
Lampart123123 <lempart.mar@gmail.com>
2020-10-10 11:47:29 +0200

    Created: 1) AboutGame activity 2) MainGameActivity activity

---
+++ app/src/main/java/com/gamesforfriends/qubicgameadventure/AboutGame.java
@@ -0,0 +1,19 @@
+package com.gamesforfriends.qubicgameadventure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_game);
+
+
+
+
+
+    }
+}
--- app/src/main/java/com/gamesforfriends/qubicgameadventure/MainActivity.java
+++ app/src/main/java/com/gamesforfriends/qubicgameadventure/MainActivity.java
@@ -2,14 +2,48 @@ package com.gamesforfriends.qubicgameadventure;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mStartgame;
+    Button mAbout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+     mStartgame = findViewById(R.id.start);
+     mAbout = findViewById(R.id.about);
+
+
+     // transition to MainGameActivity (main game space)
+        mStartgame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent_game = new Intent(MainActivity.this, MainGameActivity.class);
+                startActivity(intent_game);
+            }
+        });
+
+        // transition to AboutActivity (game description, about authors)
+        mAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent_about = new Intent(MainActivity.this, AboutGame.class);
+                startActivity(intent_about);
+            }
+
+        });
+
+
+
 
 
     }
